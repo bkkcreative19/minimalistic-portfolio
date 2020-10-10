e5931e8bdf83a77613864fdc1a759eefdb79dfe7
Baron King <bkkcreative17@gmail.com>
2020-10-10 16:36:49 -0500

    added interested section

---
+++ src/components/Interested/Interested.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Interested.scss'
+
+const Interested = () => {
+    return (
+        <section className='interested'>
+            <h3 className='interested__head'>Interested in doing</h3>
+            <button className='btn interested__head-btn'>Contact Me</button>
+        </section>
+    )
+}
+
+export default Interested
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import About from '../../components/About/About'
 import Hero from '../../components/Hero/Hero'
+import Interested from '../../components/Interested/Interested'
 import NavBar from '../../components/NavBar/NavBar'
 import './Home.scss'
 
@@ -10,6 +11,7 @@ const Home = () => {
             <NavBar/>
             <Hero/>
             <About/>
+            <Interested/>
         </div>
     )
 }
