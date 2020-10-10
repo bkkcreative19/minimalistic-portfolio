a1e8df0ed07d31647467f472cbbc3c0df4f30d6c
Baron King <bkkcreative17@gmail.com>
2020-10-10 13:39:41 -0500

    inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+     hi 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <nav className='nav-bar'>
+            <div className="nav-bar__logo"></div>
+            <ul className="nav-bar__list">
+                
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
