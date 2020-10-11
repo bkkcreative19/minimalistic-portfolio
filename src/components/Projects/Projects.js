bea5a9de94dc0c14a6eaa12870da49e0aaf5b1d2
Baron King <bkkcreative17@gmail.com>
2020-10-10 22:58:19 -0500

    finished projects page

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './Projects.scss'
+
+const Projects = () => {
+    return (
+        <section className='projects'>
+            <div className="projects__item">
+                <div className="projects__item-img img1"></div>
+                <div className="projects__item-content">
+                    <div className="projects__item-content-container">
+                        <h3 className="projects__item-content-head">Manage</h3>
+                        <p className="projects__item-content-text"> This project required me to build a fully responsive landing page to the designs provided. 
+                        I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, 
+                        such as the testimonial slider.</p>
+                        <button className="projects__item-content-btn btn">View Project</button>
+                    </div>
+                    <div className="border-top"></div>
+                    <div className="border-bottom"></div>
+                </div>
+            </div>
+            <div className="projects__item">
+                <div className="projects__item-img img2"></div>
+                <div className="projects__item-content">
+                    <div className="projects__item-content-container">
+                        <h3 className="projects__item-content-head">Bookmark</h3>
+                        <p className="projects__item-content-text">This project required me to build a fully responsive landing page to the designs provided. I 
+                        used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such 
+                        as the features section.</p>
+                        <button className="projects__item-content-btn btn">View Project</button>
+                    </div>
+                    <div className="border-top"></div>
+                    <div className="border-bottom"></div>
+                </div>
+            </div>
+            <div className="projects__item">
+                <div className="projects__item-img img3"></div>
+                <div className="projects__item-content">
+                    <div className="projects__item-content-container">
+                        <h3 className="projects__item-content-head">Insure</h3>
+                        <p className="projects__item-content-text">This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive 
+                        landing page. The only JavaScript this project required was to enable the toggling of the 
+                        mobile navigation.</p>
+                        <button className="projects__item-content-btn btn">View Project</button>
+                    </div>
+                    <div className="border-top"></div>
+                    <div className="border-bottom"></div>
+                </div>
+            </div>
+            <div className="projects__item">
+                <div className="projects__item-img img4"></div>
+                <div className="projects__item-content">
+                    <div className="projects__item-content-container">
+                        <h3 className="projects__item-content-head">Fylo</h3>
+                        <p className="projects__item-content-text">This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and 
+                        built it so that it was fully-responsive. I took a mobile-first approach and used modern 
+                        CSS like Flexbox and Grid for layout purposes.</p>
+                        <button className="projects__item-content-btn btn">View Project</button>
+                    </div>
+                    <div className="border-top"></div>
+                    <div className="border-bottom"></div>
+                </div>
+            </div>
+            
+        </section>
+    )
+}
+
+export default Projects
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from '../../components/NavBar/NavBar'
+import Projects from '../../components/Projects/Projects'
 
 const Portfolio = () => {
     return (
         <div className='portfolio container'>
             <NavBar/>
-            portfolio
+            <Projects/>
         </div>
     )
 }
